522c04e996d0a20d3cb028064dffe4717e0d36db
jonesrya <jonesrya001@isd284.com>
2025-03-19 12:49:36 -0500

    Update toggle-mode.tsx

--- components/toggle-mode.tsx
+++ components/toggle-mode.tsx
@@ -11,7 +11,15 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
+
+  React.useEffect(() => {
+    // Load theme from localStorage on component mount
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme) {
+      setTheme(storedTheme);
+    }
+  }, [setTheme]);
 
   const handleThemeChange = (newTheme) => {
     switch (newTheme) {
@@ -37,7 +45,9 @@ export function ModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          {/* ... (rest of your JSX remains the same) ... */}
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
